3ca5f723e845cc06bce798c812df6e1456e55424
Mohamed Salah <mohammedsalah0858@gmail.com>
2025-12-22 12:58:08 +0200

    completing the missing queries

--- TASK day4.sql
+++ TASK day4.sql
@@ -1,4 +1,6 @@
-﻿
+﻿USE StoreDB;
+
+--1
 select *, 
 case
     when list_price<300 then 'Economy'
@@ -9,7 +11,7 @@ case
 	from production.products
 	order by list_price ;
 
-
+--2
 select *, 
 case
     when order_status=1 then 'order recieved'
@@ -19,6 +21,7 @@ case
 	end as category
 	from sales.orders;
 
+--3
 select *
         ,case
              when [orders handeled] =0 then 'new employee'
@@ -31,12 +34,15 @@ select o.staff_id ,isnull(count(o.staff_id),0) as [orders handeled] from sales.o
 on s.staff_id=o.staff_id
 group by o.staff_id) t
 
+--4
 select isnull(phone,'no phone number')
 from sales.customers;
 
 select coalesce(phone,email,'no contact') as 'prefered contact'
 from sales.customers;
 
+
+--5
 select product_name,p.product_id,
       case 
 	      when s.quantity between 1 and 10 then 'low stock'
@@ -49,3 +55,23 @@ on s.product_id=p.product_id
 inner join sales.stores store
 on s.store_id=store.store_id
 where store.store_id=1
+
+--6
+SELECT CONCAT(COALESCE(street,''),' ',COALESCE(city,''),' ',COALESCE(state,''),' ',COALESCE(zip_code,'')) AS Full_address
+from sales.customers;
+
+--7
+
+
+with total_sum as(
+select o.order_id,SUM(oi.quantity*oi.list_price*(1-oi.discount)) as total_per_order
+from sales.order_items oi inner join sales.orders o
+on o.order_id=oi.order_id
+group by o.order_id
+)
+SELECT c.customer_id ,concat(c.first_name,' ',c.last_name) cust_name ,o.order_id,t.total_per_order
+from sales.customers c inner join sales.orders o
+on c.customer_id=o.customer_id
+inner join total_sum t
+on o.order_id=t.order_id;
+
